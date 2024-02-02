38630eb25e5a3348b105d3aafe819bd69efd3e1b
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-02-02 13:58:48 -0800

    Inquiry Check & Send Email

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.mail import send_mail
 from .models import Contact
 
 def contact(request):
@@ -12,6 +13,14 @@ def contact(request):
         message = request.POST['message']
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
+        
+        # Check if user has made an inquiry already
+        if request.user.is_authenticated: 
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
+            if has_contacted:
+                messages.error(request, 'You Have Already Made An Inquiry For This Listing')
+                return redirect('/listings/' + listing_id)
         
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         
